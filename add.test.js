fb1deb48b936a03f11852c92da3bf66d8aa1722f
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-06 23:03:18 +0530

    test: added one more test to avoid false positives

--- add.test.js
+++ add.test.js
@@ -3,4 +3,6 @@ const add = require("./add");
 test("Should return sum of two numbers.", () => {
     let res = add(1,2);
     expect(res).toBe(3);
+    res = add(3,10);
+    expect(res).toBe(13);
 });
